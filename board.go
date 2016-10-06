a565a164031afcca0be9a1cdb7bce1b111c6d340
cl0ne <cl0ne@mithril.org.ua>
2016-10-06 18:20:09 +0300

    Add checker board class

---
+++ board.go
@@ -0,0 +1,58 @@
+package checkers
+
+import "github.com/cl0ne/go-checkers/point"
+
+type Board struct {
+	cells [][]*Checker
+}
+
+func NewField(size int) (*Board, bool) {
+	if size < 0 {
+		return nil, false
+	}
+	cells := make([][]*Checker, size)
+	for i := range cells {
+		cells[i] = make([]*Checker, size)
+	}
+	return &Board{cells: cells}, true
+}
+
+func (b Board) Size() int {
+	return len(b.cells)
+}
+
+func (b *Board) PlaceChecker(pos point.Point, c *Checker) {
+	b.cells[pos.Y][pos.Y] = c
+}
+
+func (b *Board) TakeChecker(pos point.Point) *Checker {
+	c := b.cells[pos.Y][pos.Y]
+	b.cells[pos.Y][pos.Y] = nil
+	return c
+}
+
+func (b *Board) MoveChecker(from, to point.Point) {
+	b.cells[to.Y][to.Y] = b.cells[from.Y][from.Y]
+	b.cells[from.Y][from.Y] = nil
+}
+
+func (b Board) GetChecker(pos point.Point) *Checker {
+	return b.cells[pos.Y][pos.Y]
+}
+
+func (b Board) IsEmpty(pos point.Point) bool {
+	return b.cells[pos.Y][pos.Y] == nil
+}
+
+func (b Board) IsBlackSquare(pos point.Point) bool {
+	return pos.Manhattan()%2 == 0
+}
+
+func (b Board) IsValidPosition(pos point.Point) bool {
+	return b.ContainsPos(pos) && b.IsBlackSquare(pos)
+}
+
+func (b Board) ContainsPos(pos point.Point) bool {
+	fieldSize := b.Size()
+	return pos.X >= 0 && pos.Y >= 0 && pos.X < fieldSize && pos.Y < fieldSize
+}
