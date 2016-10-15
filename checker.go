aeb39278596da96bb5d1d8d22b4a8214da49682f
cl0ne <cl0ne@mithril.org.ua>
2016-10-15 03:10:43 +0300

    Change Checker interface: use coordinates instead of Points

--- checker.go
+++ checker.go
@@ -12,15 +12,19 @@ type Checker struct {
 func newChecker(x, y int, isWhite bool) Checker {
 	return Checker{
 		position: point.Point{X: x, Y: y},
-		color:    isWhite, queen: false, alive: true}
+		color:    isWhite,
+		queen:    false,
+		alive:    true,
+	}
 }
 
 func (ch Checker) Position() point.Point {
 	return ch.position
 }
 
-func (ch *Checker) setPosition(p point.Point) {
-	ch.position = p
+func (ch *Checker) setPosition(x, y int) {
+	ch.position.X = x
+	ch.position.Y = y
 }
 
 func (ch Checker) IsWhite() bool {
--- checker_test.go
+++ checker_test.go
@@ -1,61 +1,62 @@
 package checkers
 
 import (
-  "testing"
-  "github.com/cl0ne/go-checkers/point"
+	"testing"
+
+	"github.com/cl0ne/go-checkers/point"
 )
 
 func TestKill(t *testing.T) {
-  checker := newChecker(2, 2, true)
-  checker.kill()
-  if checker.IsAlive() {
-    t.Error("checker is alive after kill.")
-  }
+	checker := newChecker(2, 2, true)
+	checker.kill()
+	if checker.IsAlive() {
+		t.Error("checker is alive after kill.")
+	}
 }
 
 func TestNewChecker(t *testing.T) {
-  checker_queen := newChecker(3, 2, false)
-  checker_alive := newChecker(2, 4, true)
-  checker_pos := newChecker(4, 2, true)
-  point := point.Point{ X:4, Y:2 }
-  if checker_queen.IsQueen() {
-    t.Error("checker is queen.")
-  }
-  if !checker_alive.IsAlive() {
-    t.Error("checker was killed after creation.")
-  }
-  if checker_pos.Position() != point {
-    t.Error("checker is not right position.")
-  }
+	checker_queen := newChecker(3, 2, false)
+	checker_alive := newChecker(2, 4, true)
+	checker_pos := newChecker(4, 2, true)
+	point := point.Point{X: 4, Y: 2}
+	if checker_queen.IsQueen() {
+		t.Error("checker is queen.")
+	}
+	if !checker_alive.IsAlive() {
+		t.Error("checker was killed after creation.")
+	}
+	if checker_pos.Position() != point {
+		t.Error("checker is not right position.")
+	}
 }
 
 func TestMakeQueen(t *testing.T) {
-  checker := newChecker(3, 3, true)
-  checker.makeQueen()
-  if !checker.IsQueen() {
-    t.Error("checker is not queen.")
-  }
+	checker := newChecker(3, 3, true)
+	checker.makeQueen()
+	if !checker.IsQueen() {
+		t.Error("checker is not queen.")
+	}
 }
 
 func TestIsWhite(t *testing.T) {
-  checker := newChecker(3, 3, true)
-  if checker.IsBlack() {
-    t.Error("checker is black.")
-  }
+	checker := newChecker(3, 3, true)
+	if checker.IsBlack() {
+		t.Error("checker is black.")
+	}
 }
 
 func TestIsBlack(t *testing.T) {
-  checker := newChecker(3, 3, false)
-  if checker.IsWhite() {
-    t.Error("checker is white.")
-  }
+	checker := newChecker(3, 3, false)
+	if checker.IsWhite() {
+		t.Error("checker is white.")
+	}
 }
 
 func TestSetPosition(t *testing.T) {
-  checker := newChecker(4, 5, true)
-  point := point.Point{ X:6, Y:2 }
-  checker.setPosition(point)
-  if checker.Position() != point {
-    t.Error("checker is not right position.")
-  }
+	checker := newChecker(4, 5, true)
+	point := point.Point{X: 6, Y: 2}
+	checker.setPosition(point.X, point.Y)
+	if checker.Position() != point {
+		t.Error("checker is not right position.")
+	}
 }
