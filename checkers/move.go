6d58e94977400501c09fc1c7ceaa29bc524583ef
cl0ne <cl0ne@mithril.org.ua>
2016-10-25 19:44:19 +0300

    Add captured checker position accessor for Move

--- checkers/move.go
+++ checkers/move.go
@@ -11,3 +11,12 @@ type Move struct {
 func (m Move) IsCapture() bool {
 	return m.CapturedChecker != nil
 }
+
+// CapturedPos returns position of captured checker for the move
+// if it caused capture or point outside of the board otherwise.
+func (m Move) CapturedPos() Point {
+	if !m.IsCapture() {
+		return Point{-1, -1}
+	}
+	return m.CapturedChecker.Position()
+}
