e35fe0247aaa2aa0d6b8acd89180cb8553583247
cl0ne <cl0ne@mithril.org.ua>
2016-10-25 19:44:06 +0300

    Document Move

--- checkers/move.go
+++ checkers/move.go
@@ -1,11 +1,13 @@
 package checkers
 
+// Move describes move for checker
 type Move struct {
 	Target          Point
 	CapturedChecker *Checker
 	BecomeQueen     bool
 }
 
+// IsCapture reports whether the move caused capture.
 func (m Move) IsCapture() bool {
 	return m.CapturedChecker != nil
 }
